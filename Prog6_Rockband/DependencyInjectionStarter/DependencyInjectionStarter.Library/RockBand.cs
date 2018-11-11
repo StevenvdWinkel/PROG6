eba0307b2c53abe03a07ae33f53aa14114507063
Steven van de Winkel <s.vandewinkel1@student.avans.nl>
2018-11-11 11:31:48 +0100

    project finished

---
+++ Prog6_Rockband/DependencyInjectionStarter/DependencyInjectionStarter.Library/RockBand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjectionStarter.Library
+{
+    public class RockBand
+    {
+        private List<IInstrument> instruments;
+
+        public RockBand(List<IInstrument> instruments)
+        {
+            this.instruments = instruments;
+        }
+
+        public void DoSoundCheck()
+        {
+            foreach (var i in instruments)
+            {
+                Console.WriteLine(i.Play());
+            }
+        }
+    }
+}
---
+++ Prog6_Rockband/DependencyInjectionStarter/DependencyInjectionStarter/Program.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInjectionStarter.Library;
+using System;
+
+namespace DependencyInjectionStarter
+{
+    class Program
+    {
+        /** Testing 123 */
+        static void Main()
+        {
+            BandLocator bandLocator = new BandLocator();
+            RockBand rockBand = bandLocator.Metallica;
+            rockBand.DoSoundCheck();
+
+            Console.ReadLine();
+        }
+    }
+}
